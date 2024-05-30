a95ebcb1138990208f2ca7f6868489cfecc79593
ErzhanSou <souverigh@gmail.com>
2024-05-30 12:44:04 -0500

    added Mapper

--- src/test/java/api/POJOPractice.java
+++ src/test/java/api/POJOPractice.java
@@ -1,5 +1,7 @@
 package api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -28,5 +30,72 @@ public class POJOPractice {
 
         int statusCode = response.getStatusCode();
         Assert.assertEquals(201, statusCode);
+    }
+
+    @Test
+    public void testCustom() throws JsonProcessingException {
+        String url = Config.getProperty("cashWiseApiUrl") + "/api/myaccount/categories";
+        String token = CashwiseAuthorization.getToken();
+        RequestBody requestBody = new RequestBody();
+        requestBody.setCategory_title("education");
+        requestBody.setCategory_description("Udemy.com");
+        requestBody.setFlag(true);
+
+        Response response = RestAssured.given()
+                .auth()
+                .oauth2(token)
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .post(url);
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(201, statusCode);
+        response.prettyPrint();
+
+        ObjectMapper mapper = new ObjectMapper();
+        CustomResponse customResponse = mapper.readValue(response.asString(), CustomResponse.class);
+        String expectedTitle = customResponse.getCategory_title();
+        String expectedCategory_description = customResponse.getCategory_description();
+        Assert.assertEquals(expectedTitle, "education");
+        Assert.assertEquals(expectedCategory_description, "Udemy.com");
+    }
+
+
+    @Test
+    public void createGetSeller() throws JsonProcessingException {
+        String url = Config.getProperty("cashWiseApiUrl") + "/api/myaccount/sellers";
+        String token = CashwiseAuthorization.getToken();
+
+        RequestBody requestBody = new RequestBody();
+        requestBody.setCompany_name("Tesla");
+        requestBody.setSeller_name("Elon Musk1");
+        requestBody.setPhone_number("+13129099999");
+        requestBody.setEmail("elon3@gmail.com");
+        requestBody.setAddress("18888 Dev Ave");
+
+        Response response = RestAssured.given()
+                .auth()
+                .oauth2(token)
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .post(url);
+        response.prettyPrint();
+
+        ObjectMapper mapper = new ObjectMapper();
+        CustomResponse customResponse = mapper.readValue(response.asString(), CustomResponse.class);
+        int id = customResponse.getSeller_id();
+
+        String urlGetSingleSeller = Config.getProperty("cashWiseApiUrl") + "/api/myaccount/sellers/" + id;
+        Response response1 = RestAssured.given()
+                .auth()
+                .oauth2(token)
+                .get(urlGetSingleSeller);
+        int statusCode = response1.statusCode();
+        Assert.assertEquals(200, statusCode);
+        String expectedSellerName = customResponse.getSeller_name();
+        String expectedCompanyName = customResponse.getCompany_name();
+        Assert.assertEquals(expectedSellerName, "Elon Musk1");
+        Assert.assertEquals(expectedCompanyName, "Tesla");
+
     }
 }
--- src/test/java/api/RequestBody.java
+++ src/test/java/api/RequestBody.java
@@ -1,5 +1,9 @@
 package api;
 
+import lombok.Data;
+
+@Data
+
 public class RequestBody {
     private String email;
     private String password;
@@ -8,79 +12,15 @@ public class RequestBody {
     private String description;
     private int balance;
     private String category_title;
-
-    public String getCategory_description() {
-        return category_description;
-    }
-
-    public void setCategory_description(String category_description) {
-        this.category_description = category_description;
-    }
-
-    public String getCategory_title() {
-        return category_title;
-    }
-
-    public void setCategory_title(String category_title) {
-        this.category_title = category_title;
-    }
-
-    public boolean isFlag() {
-        return flag;
-    }
-
-    public void setFlag(boolean flag) {
-        this.flag = flag;
-    }
-
     private String category_description;
     private boolean flag;
+    private String company_name;
+    private String seller_name;
+    private String phone_number;
+    private String address;
 
-    public String getType_of_pay() {
-        return type_of_pay;
-    }
 
-    public void setType_of_pay(String type_of_pay) {
-        this.type_of_pay = type_of_pay;
-    }
+    /*
 
-    public String getBank_account_name() {
-        return bank_account_name;
-    }
-
-    public void setBank_account_name(String bank_account_name) {
-        this.bank_account_name = bank_account_name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getBalance() {
-        return balance;
-    }
-
-    public void setBalance(int balance) {
-        this.balance = balance;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+     */
 }
