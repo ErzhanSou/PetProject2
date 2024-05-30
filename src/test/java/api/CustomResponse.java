8dfab875e5ec73c3a8e575f57b9be7e9e7b0482c
ErzhanSou <souverigh@gmail.com>
2024-05-30 12:44:09 -0500

    added Mapper

---
+++ src/test/java/api/CustomResponse.java
@@ -0,0 +1,17 @@
+package api;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class CustomResponse {
+    private String category_title;
+    private int category_id;
+    private String created;
+    private String category_description;
+    private int seller_id;
+    private String seller_name;
+    private String company_name;
+}
